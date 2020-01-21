22903ed36513269276aefcea7f14a16fe78ed537
minjeong.koo <koominjeong99@gmail.com>
2020-01-21 15:56:12 +0900

    CLOSE Mobile Responsive CSS

--- front/src/main/resources/static/controller/frame/Layout.controller.js
+++ front/src/main/resources/static/controller/frame/Layout.controller.js
@@ -13,28 +13,6 @@ sap.ui.define([
 			
 			console.log("Layout.js OnInit()");
 			
-			// CHECK DEVICE TYPE AND SETTING FULL SCREEN MODE.
-			
-			var deviceFilter = "win16|win32|win64|macintel|mac|"
-		
-			if( navigator.platform) {
-
-			    if( deviceFilter.indexOf(navigator.platform.toLowerCase())<0 ) {
-			
-			    	// "MOBILE" DEVICE TYPE + SETTING META INFO
-			        var metaInfo 		= document.getElementsByTagName('meta').viewport;
-			        metaInfo.content 	= "width=device-width, initial-scale=0.5, user-scalable=no"; 
-			        
-			    }
-			    else {
-			    	
-			    	// "PC" 	DEVICE TYPE + SETTING META INFO
-			    	var fullScreenArea 	= document.getElementById('content');
-			        this.fullscreen 	= fullScreenArea;
-			        /* this.layoutControll.fullscreen.requestFullscreen(); */ 
-			        
-			    }
-			}
 		},
 		
 		onAfterRendering : function () {
@@ -42,6 +20,31 @@ sap.ui.define([
 			
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("contents1");
+			
+			// CHECK DEVICE TYPE AND SETTING FULL SCREEN MODE.
+			
+			var deviceFilter = "win16|win32|win64|macintel|mac|"
+				
+				if( navigator.platform) {
+
+				    if( deviceFilter.indexOf(navigator.platform.toLowerCase())<0 ) {
+				
+				    	// "MOBILE" DEVICE TYPE + SETTING META INFO
+				        var metaInfo 		= 	document.getElementsByTagName('meta').viewport;
+				        metaInfo.content 	= 	"width=device-width, initial-scale=0.5, user-scalable=no";
+				        
+				        
+				    }
+				    else {
+				    	
+				    	// "PC" 	DEVICE TYPE + SETTING META INFO
+				    	var fullScreenArea 	= 	document.getElementById('content');
+				        this.fullscreen 	= 	fullScreenArea;
+				        /*this.layoutControll.fullscreen.requestFullscreen(); */ 
+				    	
+				    }
+				}
+			
 			
 		},
 		
