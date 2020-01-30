88eefc4cb35bd5699e5e1996c98b220b9523ec7f
KSJ <KSJ@DESKTOP-62UK71H>
2020-01-30 12:49:24 +0900

    .

--- front/src/main/java/com/tips/front/FrontApplication.java
+++ front/src/main/java/com/tips/front/FrontApplication.java
@@ -2,9 +2,12 @@ package com.tips.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-public class FrontApplication {
+@Controller
+public class FrontApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontApplication.class, args);
