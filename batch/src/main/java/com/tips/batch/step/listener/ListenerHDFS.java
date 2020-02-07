b0785ce1dfeb0b3d95d50cd6af19f12a4845982e
leehyounkyoo <leehyounkyoo@gmail.com>
2020-02-07 17:59:11 +0900

    hadoop_pseudo_cluster 에 write 하는 listener 추가

--- batch/src/main/java/com/tips/batch/step/listener/ListenerHDFS.java
+++ batch/src/main/java/com/tips/batch/step/listener/ListenerHDFS.java
@@ -43,47 +43,78 @@ public class ListenerHDFS extends JobExecutionListenerSupport
             
             String createTime = simpleDateFormat.format(date);
             
+            String filename = "measure_info_real_" + createTime + ".csv";
+            
             try 
             {
-                //Configuration conf = new Configuration();
+                Configuration conf = new Configuration();
+
+                // 50010 포트에서 사용하는 IP에 hostname 을 사용하도록 설정
+                // 도커와 같은 가상IP 환경일 경우 실제 IP 를 찾기 때문에 파일은 생성이 되어도 datanode 에 쓸 때 IP를 찾기 못한다.
+                conf.set("dfs.client.use.datanode.hostname", "true");
                 
-                //Path file = new Path("file:////localhost:9000/tmp/test/test-camel-simple-write-file1");
-                FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.29:50070"), new Configuration());
-                //FileSystem fs = FileSystem.getLocal(conf);
+                // 서버는 아래와 같이 설정
+//            <configuration>
+//                <property>
+//                    <name>dfs.replication</name>
+//                    <value>1</value>  // 단일 namenode, datanode
+//                    <final>true</final>
+//                </property>
+//                <property>
+//                    <name>dfs.namenode.name.dir</name>
+//                    <value>/hadoop_home/namenode_home</value>
+//                    <final>true</final>
+//                </property>
+//                <property>
+//                    <name>dfs.datanode.data.dir</name>
+//                    <value>/hadoop_home/datanode_home</value>
+//                    <final>true</final>
+//                </property>
+//                <property>
+//                        <name>dfs.client.use.datanode.hostname</name>  // client 와 맞춰워야 함
+//                        <value>true</value>
+//                </property>
+//                <property>
+//                        <name>dfs.permissions.enabled</name>
+//                        <value>false</value>
+//                </property>
+//            </configuration>
+            
+                FileSystem fs = FileSystem.get(new URI("hdfs://master:9000"), conf);
                 
-                FSDataOutputStream out = fs.create(new Path("test"));
+                FSDataOutputStream out = fs.create(new Path(filename));
                 
                 out.writeBytes(HEADER);
                 
                 for (MeasureInfoReal record : measureInfoRealListVO.get())
                 {
                     out.writeBytes(new StringBuilder().append(record.getColumnA1()).append(LINE_DILM)
-                                                        .append(record.getColumnA3()).append(LINE_DILM)
-                                                        .append(record.getColumnA5()).append(LINE_DILM)
-                                                        .append(record.getColumnA6()).append(LINE_DILM)
-                                                      //  .append(record.getColumnA7()).append(LINE_DILM)
-                                                      //  .append(record.getColumnA8()).append(LINE_DILM)
-                                                      //  .append(record.getColumnA9()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB1()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB2()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB3()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB4()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB5()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB6()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB7()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB8()).append(LINE_DILM)
-                                                      //  .append(record.getColumnB9()).append(LINE_DILM)
-                                                      //  .append(record.getColumnC1()).append(LINE_DILM)
-                                                      //  .append(record.getColumnC2()).append(LINE_DILM)
-                                                      //  .append(record.getColumnC3()).append(LINE_DILM)
-                                                      //  .append(record.getColumnC4()).append(LINE_DILM)
-                                                      //  .append(record.getColumnC5()).append(LINE_DILM)
-                                                        .append(record.getColumnC6()).toString());
+                                                      .append(record.getColumnA3()).append(LINE_DILM)
+                                                      .append(record.getColumnA5()).append(LINE_DILM)
+                                                      .append(record.getColumnA6()).append(LINE_DILM)
+                                                      .append(record.getColumnA7()).append(LINE_DILM)
+                                                      .append(record.getColumnA8()).append(LINE_DILM)
+                                                      .append(record.getColumnA9()).append(LINE_DILM)
+                                                      .append(record.getColumnB1()).append(LINE_DILM)
+                                                      .append(record.getColumnB2()).append(LINE_DILM)
+                                                      .append(record.getColumnB3()).append(LINE_DILM)
+                                                      .append(record.getColumnB4()).append(LINE_DILM)
+                                                      .append(record.getColumnB5()).append(LINE_DILM)
+                                                      .append(record.getColumnB6()).append(LINE_DILM)
+                                                      .append(record.getColumnB7()).append(LINE_DILM)
+                                                      .append(record.getColumnB8()).append(LINE_DILM)
+                                                      .append(record.getColumnB9()).append(LINE_DILM)
+                                                      .append(record.getColumnC1()).append(LINE_DILM)
+                                                      .append(record.getColumnC2()).append(LINE_DILM)
+                                                      .append(record.getColumnC3()).append(LINE_DILM)
+                                                      .append(record.getColumnC4()).append(LINE_DILM)
+                                                      .append(record.getColumnC5()).append(LINE_DILM)
+                                                      .append(record.getColumnC6()).toString());
                     
                     out.writeBytes("\n");
                 }
                 
-                out.close();;
+                out.close();
             }
             catch (Exception e)
             {
